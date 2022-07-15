dc5dbab73e7815d72c91a5be501d23e2207db7cc
Ivanopulos <Pochta.rus.lc@gmail.com>
2022-07-15 10:49:01 +0300

    tek1 ready

--- ramisbd.py
+++ ramisbd.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import os
 from tkinter import filedialog
 import dill
 import numpy as np
 
+def p(aaarg):
+    print(aaarg)
 def pdopenfail(path=""):
     # df = pd.read_csv('apple.csv', index_col='Date', parse_dates=True)
     if path == "":
@@ -32,30 +35,40 @@ def snl_session(fn="savedsession.pkl"):
         dill.dump_session(fn)
         print('session saved')
 
-#входящ_файл = pdopenfail()
-snl_session()
-res = np.argwhere(входящ_файл.values == 'Регион')
-lc = 0
-rc = 1
-if res[0][1] > res[1][1]:
-    lc = 1  # на случай смещения второй таблицы
-    rc = 0
-ltable = входящ_файл.iloc[res[lc][0]:, res[lc][1]:res[rc][1]].dropna(axis=0, how='all') # убираем строки до региона, убираем вторую таблицу, удаляем пустые строки
-ltable = ltable.dropna(axis=1, how='all')  # убираем пустые столбцы
-ltable.columns = ltable.iloc[0]  # в заголовок первую теперь уже строку
-ltable=ltable.iloc[1:]  # удалить строку переехавшую в заголовок
-rtable = входящ_файл.iloc[res[rc][0]:, res[rc][1]:].dropna(axis=0, how='all')
-rtable = rtable.dropna(axis=1, how='all')
-rtable.columns = rtable.iloc[0]
-rtable = rtable.iloc[1:]
-rtable = rtable.drop(['Регион', 'Код МО', 'Наименование структурного подразделения медицинской организации', 'Название проекта по улучшению'], axis=1)  # столбцы поп которым идет задвоение
-ltable.reset_index(drop=True, inplace=True)
-rtable.reset_index(drop=True, inplace=True)
-ltable = ltable.join(rtable)
+#vf = pdopenfail()
 
-print(ltable)
-print(res)
-print(res[lc][1])
-print(res[rc][1])
+# snl_session()
+
+# res = np.argwhere(vf.values == 'Регион')
+# lc = 0
+# rc = 1
+# if res[0][1] > res[1][1]:
+#     lc = 1  # на случай смещения второй таблицы
+#     rc = 0
+# ltable = vf.iloc[res[lc][0]:, res[lc][1]:res[rc][1]].dropna(axis=0, how='all') # убираем строки до региона, убираем вторую таблицу, удаляем пустые строки
+# ltable = ltable.dropna(axis=1, how='all')  # убираем пустые столбцы
+# ltable.columns = ltable.iloc[0]  # в заголовок первую теперь уже строку
+# ltable=ltable.iloc[1:]  # удалить строку переехавшую в заголовок
+# rtable = vf.iloc[res[rc][0]:, res[rc][1]:].dropna(axis=0, how='all')
+# rtable = rtable.dropna(axis=1, how='all')
+# rtable.columns = rtable.iloc[0]
+# rtable = rtable.iloc[1:]
+# rtable = rtable.drop(['Регион', 'Код МО', 'Наименование структурного подразделения медицинской организации', 'Название проекта по улучшению'], axis=1)  # столбцы поп которым идет задвоение
+# ltable.reset_index(drop=True, inplace=True)
+# rtable.reset_index(drop=True, inplace=True)
+# ltable = ltable.join(rtable)
+# ltable['Регион'] = ltable['Регион'].fillna(1)  # хз как зацепиться за НаН, пришлось менять на 1, чтобы продублировать название региона
+# for i in range(len(ltable)):
+#     if ltable.loc[i]['Регион'] == 1:
+#         ltable.loc[i]['Регион'] = ltable.loc[i-1]['Регион']
+# tek1s=['Регион', 'Код МО', "Наименование структурного подразделения медицинской организации", "Обслуживаемое население", "Количество работников, устроенных по основному месту работы в структурном подразделении медицинской организации, на начало календарного года", "Количество работников, из числа устроенных по основному месту работы в структурном подразделении медицинской организации, обученных методам и инструментам бережливого производства (наличие документа о повышении квалификации), на момент подачи отчета"]
+# Tek1 = ltable[tek1s] #, "Код МО", "Наименование структурного подразделения медицинской организации", "Обслуживаемое население", "Количество работников, устроенных по основному месту работы в структурном подразделении медицинской организации, на начало календарного года", "Количество работников, из числа устроенных по основному месту работы в структурном подразделении медицинской организации, обученных методам и инструментам бережливого производства (наличие документа о повышении квалификации), на момент подачи отчета"]
+# Tek1 = Tek1.groupby(tek1s).size().reset_index(name=('дублей'))
+
+snl_session("tek1")
+Tek2=
+
+p(Tek1)
+
 #writer = pd.ExcelWriter('output.xlsx')
-#ltable.to_excel('output1.xlsx')
+#Tek1.to_excel('output1.xlsx')
